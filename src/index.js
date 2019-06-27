a0c128283428434619d15c75cfda65f4ee56c402
BaharaJr <bennybenester@gmail.com>
2019-06-27 22:28:51 +0300

    :label: Added a sourcemap to index.js
    Added a sourcemap (debugger) to help see original code in dev tool

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import numeral from 'numeral';
 import './index.css';
 
 const courseValue = numeral(1080).format('$0,0.0');
+debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
