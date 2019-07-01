ae9590f3bb62e8ddc8a2f1b79bb934249b99b41d
BaharaJr <bennybenester@gmail.com>
2019-07-02 00:28:42 +0300

    :construction: Add http calls to mock data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-undef */
 import express from 'express';
-import { join } from 'path';
+import path from 'path';
 import open from 'open';
-import webpack from "webpack";
+import webpack from 'webpack';
 import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 5000;
 const app = express();
@@ -13,16 +14,24 @@ app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
-  
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '../src/index.html'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err){
-  if(err){
-    // eslint-disable-next-line no-console
-    console.log(error)
-  } else{
-    open('http://localhost:' +port);
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
   }
 });
